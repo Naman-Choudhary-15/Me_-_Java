41d009a0ee6d6929289a942fb1b5c652268365d8
Naman Choudhary <choudharynaman150@gmail.com>
2026-07-27 12:10:51 +0530

    how to take input

---
+++ takinginput.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class takinginput {
+    public static void main(String[] args) {
+        //Input
+        // one input only
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.next();
+        // System.out.println(name);
+        
+        //taking line as a input 
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
