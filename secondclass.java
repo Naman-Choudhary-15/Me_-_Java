31cb9e04ae39da087288c06121f81a7e63f41c21
Naman Choudhary <choudharynaman150@gmail.com>
2026-07-27 12:07:45 +0530

    second class

---
+++ secondclass.java
@@ -0,0 +1,33 @@
+public class secondclass {
+    public static void main(String[] args) {
+
+        // printing a pattern of *
+
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+    
+        // **Variables**
+    
+        // String name = "Mohit kumar";
+        // int age = 31;
+        // double price = 30.23;
+        // int a = 10;
+        // int b = 29;
+    
+        //     b = 22;
+        //     name = "Rohit Kumar";
+        // System.out.println(name);
+        // System.out.println(age);
+        // System.out.println(a+b+price);
+
+        int a = 15;
+        int b = 20;
+        int sum = a+b;
+
+        System.out.println(sum);
+        System.out.println(a-b);
+        System.out.println(a*b);
+    }
+}
