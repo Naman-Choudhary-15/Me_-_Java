2dd0743be9c047329e97a65b09cf9db52176f897
Naman Choudhary <choudharynaman150@gmail.com>
2026-07-29 19:40:41 +0530

    Loops in java

---
+++ Loops.java
@@ -0,0 +1,40 @@
+public class Loops {
+    public static void main(String[] args) {
+        
+        // FOR LOOP
+
+        //for(int i=0; i <3 ; i=i+1)
+        //    System.out.println("Hello World");
+
+        // for(int i = 0; i<=10; i++) // i++ => i = i + 1
+        //     System.out.println(i);
+
+        // WHILE LOOP
+
+        // int i = 1;
+        // while (i <= 10) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // DO WHILE LOOP
+        // int i = 0;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i<=10);
+
+
+        // False Cases to Check the working 
+        int i = 11;
+        while (i<=10) {
+            System.out.println(i);
+            i++;
+        }
+
+        do{
+            System.out.println(i);
+            i++;
+        } while (i<= 10);
+    }
+}
