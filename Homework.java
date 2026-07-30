b19dde06039787ba2e433b8d696285f5ebb1b5b4
Naman Choudhary <choudharynaman150@gmail.com>
2026-07-30 23:05:54 +0530

    more question added

--- Homework.java
+++ Homework.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Homework {
     public static void main(String[] args) {
+
+        //**LECTURE 3 
+
         //QUESTION 1 
 
         // Scanner sc = new Scanner(System.in);
@@ -30,37 +33,78 @@ public class Homework {
         // }
 
         //QUESTION 2 
-        Scanner sc = new Scanner(System.in);
-        int button = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // int button = sc.nextInt();
 
-        switch (button) {
-            case 1: System.out.println("January");
-                break;
-            case 2: System.out.println("February");
-                break;
-            case 3: System.out.println("March");
-                break;
-            case 4: System.out.println("April");
-                break;
-            case 5: System.out.println("May");
-                break;
-            case 6: System.out.println("June");
-                break;
-            case 7: System.out.println("July");
-                break;
-            case 8: System.out.println("August");
-                break;
-            case 9: System.out.println("September");
-                break;
-            case 10: System.out.println("October");
-                break;
-            case 11: System.out.println("November");
-                break;
-            case 12: System.out.println("December");
-                break;
+        // switch (button) {
+        //     case 1: System.out.println("January");
+        //         break;
+        //     case 2: System.out.println("February");
+        //         break;
+        //     case 3: System.out.println("March");
+        //         break;
+        //     case 4: System.out.println("April");
+        //         break;
+        //     case 5: System.out.println("May");
+        //         break;
+        //     case 6: System.out.println("June");
+        //         break;
+        //     case 7: System.out.println("July");
+        //         break;
+        //     case 8: System.out.println("August");
+        //         break;
+        //     case 9: System.out.println("September");
+        //         break;
+        //     case 10: System.out.println("October");
+        //         break;
+        //     case 11: System.out.println("November");
+        //         break;
+        //     case 12: System.out.println("December");
+        //         break;
             
-            default:System.out.println("Invalid Button");
-        }
+        //     default:System.out.println("Invalid Button");
+        // }
+
+
+        //**LECTURE 4
+
+        //QUESTION 1 - Print all even numbers till n
+
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
+        // for(int i = 0; i<=n; i ++) {
+        //     if(i%2 == 0) {
+        //     System.out.println(i);
+        //     }
+        // }
+
+        //QUESTION 2 - 
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+
+    //     if(n == 0) {
+    //     Scanner scc = new Scanner(System.in);
+    //     int i = scc.nextInt();
+    //     do {
+    //         if (i>=90) {
+    //             System.out.println("This is Good");
+    //         } else if (89>=i && i>=60) {
+    //                 System.out.println("This is also Good");
+    //         } else if (59>=i && i>=0) {
+    //             System.out.println("This is Good as well, Because marks do not matter but our effort does");
+    //         }
+            
+    //     }while (i<=1);
+    // } else {
+    //     System.out.println("Stop");
+    // }
+
+
+    // QUESTION 3 - 
+    //  Print if a number is prime or not (Input n from the user). 
+
+    
+
     }
 }
-
