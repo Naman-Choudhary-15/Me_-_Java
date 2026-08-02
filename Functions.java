969741a94e652629f601680dd57442a75e6c78fa
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-02 23:56:01 +0530

    little upgrade

--- Functions.java
+++ Functions.java
@@ -1,3 +1,5 @@
 public class Functions {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- Patterns2.java
+++ Patterns2.java
@@ -1,3 +1,5 @@
 public class Patterns2 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
