742da7bebd2aab6698acda49df5157bf5b0cf699
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-03 23:59:41 +0530

    Butterfly Pattern

--- Patterns2.java
+++ Patterns2.java
@@ -1,6 +1,47 @@
 public class Patterns2 {
     public static void main(String[] args) {
-        // classes on 3 august
-        // code from here 
+       
+        // BUTTERFLY PATTERN
+
+        int n = 4;
+
+        //upper half
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            } 
+            
+            int spaces = 2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lower half
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            } 
+            
+            int spaces = 2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
     }
 }
+ 
