230be9fdbd16bcb1980e25041a86e688664d3292
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-05 23:47:28 +0530

    some changes

--- Patterns2.java
+++ Patterns2.java
@@ -3,42 +3,42 @@ public class Patterns2 {
        
         // BUTTERFLY PATTERN
 
-        // int n = 4;
+        int n = 4;
 
-        // //upper half
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=i;j++){
-        //         System.out.print("*");
-        //     } 
+        //upper half
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            } 
             
-        //     int spaces = 2*(n-i);
-        //     for(int j=1;j<=spaces;j++){
-        //         System.out.print(" ");
-        //     }
-            
-
-        //     for(int j=1;j<=i;j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
-        // //lower half
-        // for(int i=n;i>=1;i--){
-        //     for(int j=1;j<=i;j++){
-        //         System.out.print("*");
-        //     } 
-            
-        //     int spaces = 2*(n-i);
-        //     for(int j=1;j<=spaces;j++){
-        //         System.out.print(" ");
-        //     }
+            int spaces = 2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
             
 
-        //     for(int j=1;j<=i;j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lower half
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            } 
+            
+            int spaces = 2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
         // Print a solid rhombus.
         
