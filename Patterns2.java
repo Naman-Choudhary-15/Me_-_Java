eb4a1a5caef616fffb38b75427130532cd76e9ba
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-06 23:32:27 +0530

    added new pattern

--- Patterns2.java
+++ Patterns2.java
@@ -85,6 +85,53 @@ public class Patterns2 {
 
     //*** Palindromic Pattern
 
+    // int n = 5;
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         System.out.print(" ");
+    //     } 
+
+    //     for(int j=i;j>=1;j--){
+    //         System.out.print(j);
+    //     }
+
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
+
+
+    //*** Diamond Pattern
+
+    int n = 4;
+    
+    //upper part
+
+    for(int i=1;i<=n;i++){        
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+
+        for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }System.out.println();
+    }
+
+    //lower part
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+
+        for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }System.out.println();
+
+    }
+
+
     }
 }
  
