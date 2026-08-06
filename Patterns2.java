8b81f2271cc0d9cc5a49bdcb4750f5b3cdd62e4a
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-06 23:09:53 +0530

    pattern added

--- Patterns2.java
+++ Patterns2.java
@@ -3,45 +3,59 @@ public class Patterns2 {
        
         // BUTTERFLY PATTERN
 
-        int n = 4;
+        // int n = 4;
 
-        //upper half
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            } 
+        // //upper half
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     } 
             
-            int spaces = 2*(n-i);
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
-            
-
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        //lower half
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            } 
-            
-            int spaces = 2*(n-i);
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
+        //     int spaces = 2*(n-i);
+        //     for(int j=1;j<=spaces;j++){
+        //         System.out.print(" ");
+        //     }
             
 
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // //lower half
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     } 
+            
+        //     int spaces = 2*(n-i);
+        //     for(int j=1;j<=spaces;j++){
+        //         System.out.print(" ");
+        //     }
+            
+
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
 
         // Print a solid rhombus.
         
+        //Main Method
+
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+
+
+
     // int n = 4;
       
     //    for(int i=n; i>=1; i--) {
