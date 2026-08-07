e1c9f9c1a35b3dc9d8bc716a5fe41e89a7119523
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-07 23:52:40 +0530

    corrected the code

--- Homework.java
+++ Homework.java
@@ -192,22 +192,22 @@ public class Homework {
     // QUESTION 3 -
     // Print a palindromic number pyramid.
 
-    int n = 5;
+    // int n = 5;
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            System.out.print(" ");
-        } 
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         System.out.print(" ");
+    //     } 
 
-        for(int j=i;j>=1;j--){
-            System.out.print(j);
-        }
+    //     for(int j=i;j>=1;j--){
+    //         System.out.print(j);
+    //     }
 
-        for(int j=1;j<=i;j++){
-            System.out.print(j);
-        }
-        System.out.println();
-    }
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
 
     
 
