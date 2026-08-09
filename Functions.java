2e0d00ce590f477623aef2220dc77d19717a7ba5
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-09 23:58:04 +0530

    few changes

--- Functions.java
+++ Functions.java
@@ -2,9 +2,8 @@ public class Functions {
     public static void main(String[] args) {
         // Learning about functions
         
-        // Functions in Java
 
         //Start From 10 Aug
-        
+
     }
 }
