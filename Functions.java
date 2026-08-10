a694fac053202d7d9a8fa1787b90f4d92fa09604
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-10 23:30:54 +0530

    systax of Function

--- Functions.java
+++ Functions.java
@@ -1,9 +1,13 @@
 public class Functions {
     public static void main(String[] args) {
 
-        //Start From 10 Aug
-
+        // public static returnType functionName(type arg1, type arg2 ...){
+          //Operation
+        // }
 
         
+
+
+
     }
 }
