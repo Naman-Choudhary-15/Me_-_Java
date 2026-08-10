799c90d0eb7b4067e541125fd79761828cee4571
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-10 23:40:43 +0530

    printing name using Function

--- Functions.java
+++ Functions.java
@@ -1,11 +1,22 @@
-public class Functions {
-    public static void main(String[] args) {
+import java.util.*;
 
-        // public static returnType functionName(type arg1, type arg2 ...){
+                        // public static returnType functionName(type arg1, type arg2 ...){
           //Operation
         // }
 
-        
+public class Functions {
+
+    public static void printMyName(String name) {
+        System.out.println(name);
+        return;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        printMyName(name); // function ko call kiya
 
 
 
