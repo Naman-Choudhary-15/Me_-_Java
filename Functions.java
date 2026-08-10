93303bc45c20fdd8e81739939b2b4452ea39bd54
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-10 23:49:25 +0530

    Calculating Sum

--- Functions.java
+++ Functions.java
@@ -6,19 +6,37 @@ import java.util.*;
 
 public class Functions {
 
-    public static void printMyName(String name) {
-        System.out.println(name);
-        return;
-    }
+    // public static void printMyName(String name) {
+    //     System.out.println(name);
+    //     return;
+    // }
 
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+    //     Scanner sc = new Scanner(System.in);
+    //     String name = sc.next();
 
-        printMyName(name); // function ko call kiya
+    //     printMyName(name); // function ko call kiya
+
+   // }
+
+    
+   //**Sum to two numbers
+
+   public static int calculateSum(int a, int b){
+    int sum = a + b;
+    return sum;
+   }
+   public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    int sum = calculateSum(a, b);
+    System.out.println("Sum of two these numbers are: " + sum);
+
+   }
 
 
-
-    }
 }
