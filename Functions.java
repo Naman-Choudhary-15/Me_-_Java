389b3d85e9e23abd885be7cfa74b4e70e937bb5e
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-11 00:02:16 +0530

    Factorial of a Number

--- Functions.java
+++ Functions.java
@@ -41,19 +41,44 @@ public class Functions {
 
        //**Product of two numbers
 
-       public static int product(int a, int b){
-        int pro= a*b;
-        return pro;
+    //    public static int product(int a, int b){
+    //     int pro= a*b;
+    //     return pro;
+    //    }
+
+    //    public static void main(String[] args){
+
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+
+    //     int pro = product(a, b);
+    //     System.out.println("Product of these two numbers is: " + pro);
+    //    }
+
+
+    //**Factorial of a number
+
+       public static void printFactorial(int n){
+       
+        int Factorial =1;
+
+        if(n<0){
+            System.out.println("Invalid Number");
+            return;
+        }
+        for(int i=n;i>=1;i--){
+            Factorial = Factorial * i;
+        }
+        System.out.println(Factorial);
        }
 
        public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        int pro = product(a, b);
-        System.out.println("Product of these two numbers is: " + pro);
+        int n = sc.nextInt();
+        
+        printFactorial(n);
        }
 
 
