4d1cc0a2069de0e05f4be1fdad6cf6ecfc103d15
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-11 12:48:53 +0530

    Hollow Butterfly pattern added

--- Homework.java
+++ Homework.java
@@ -209,7 +209,52 @@ public class Homework {
     //     System.out.println();
     // }
 
-    
+    //  ***MORE LOOPS PROBLEM***
+
+    // Print a hollow Butterfly
+
+    int n =5;
+
+    for(int i=1;i<=n;i++){ 
+        for(int j=1;j<=i;j++){
+            if(i==j || j==1 ){
+            System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        for(int j=2*(n-i);j>=1;j--){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+            if(i==j || j==1 ){
+            System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+        }System.out.println();  
+        }
+
+    for(int i=n;i>=1;i--){ 
+        for(int j=1;j<=i;j++){
+            if(i==j || j==1 ){
+            System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        for(int j=2*(n-i);j>=1;j--){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+            if(i==j || j==1 ){
+            System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+        }System.out.println();  
+        }
+
 
     }
 }
