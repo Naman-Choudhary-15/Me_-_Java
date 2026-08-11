75a1484eb78797a6f208af478a722b2a0b255e06
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-11 17:04:13 +0530

    hollow rhombus

--- Homework.java
+++ Homework.java
@@ -213,47 +213,68 @@ public class Homework {
 
     // Print a hollow Butterfly
 
-    int n =5;
+    // int n =5;
 
-    for(int i=1;i<=n;i++){ 
-        for(int j=1;j<=i;j++){
-            if(i==j || j==1 ){
-            System.out.print("*");
-            } else {
-                System.out.print(" ");
-            }
-        }
-        for(int j=2*(n-i);j>=1;j--){
-            System.out.print(" ");
-        }
-        for(int j=1;j<=i;j++){
-            if(i==j || j==1 ){
-            System.out.print("*");
-            } else {
-                System.out.print(" ");
-            }
-        }System.out.println();  
-        }
+    // for(int i=1;i<=n;i++){ 
+    //     for(int j=1;j<=i;j++){
+    //         if(i==j || j==1 ){
+    //         System.out.print("*");
+    //         } else {
+    //             System.out.print(" ");
+    //         }
+    //     }
+    //     for(int j=2*(n-i);j>=1;j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         if(i==j || j==1 ){
+    //         System.out.print("*");
+    //         } else {
+    //             System.out.print(" ");
+    //         }
+    //     }System.out.println();  
+    //     }
 
-    for(int i=n;i>=1;i--){ 
-        for(int j=1;j<=i;j++){
-            if(i==j || j==1 ){
-            System.out.print("*");
-            } else {
-                System.out.print(" ");
-            }
-        }
-        for(int j=2*(n-i);j>=1;j--){
-            System.out.print(" ");
-        }
-        for(int j=1;j<=i;j++){
-            if(i==j || j==1 ){
-            System.out.print("*");
-            } else {
-                System.out.print(" ");
-            }
-        }System.out.println();  
-        }
+    // for(int i=n;i>=1;i--){ 
+    //     for(int j=1;j<=i;j++){
+    //         if(i==j || j==1 ){
+    //         System.out.print("*");
+    //         } else {
+    //             System.out.print(" ");
+    //         }
+    //     }
+    //     for(int j=2*(n-i);j>=1;j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         if(i==j || j==1 ){
+    //         System.out.print("*");
+    //         } else {
+    //             System.out.print(" ");
+    //         }
+    //     }System.out.println();  
+    //     }
+
+
+    // Hollow Rhombus
+
+    // int n=5;
+
+    // for(int i=5;i>=1;i--){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(" ");
+    // }
+    //     for(int j=1;j<=n;j++){
+    //         if(j==1 || j==n || i==1 || i==n) {
+    //         System.out.print("*"); 
+    //     } else { System.out.print(" ");}
+    //     }System.out.println();
+
+    // }
+
+
+    
+
 
 
     }
