0ce02c60ce1747c5e74acf30f5e677241ee38228
Naman Choudhary <choudharynaman150@gmail.com>
2026-08-17 21:58:33 +0530

    question removed

--- Homework.java
+++ Homework.java
@@ -308,9 +308,17 @@ public class Homework {
     
 
     // Make a function to check if a number is prime or not.
+
+
+
     // Make a function to check if a given number n is even or not.
+
+
+
     // Make a function to print the table of a given number n.
-    // Read about Recursion.
+
+
+
 
     
 
